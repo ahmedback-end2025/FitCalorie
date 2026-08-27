bb418273e8732b3faf88f015ec92d33438f573a1
Ahmed Saeed <Ahmed Saeed@SHS5CD9504LW8C>
2026-08-27 16:59:06 +0300

    feat : add LoginViewModel , RegisterViewModel ,add Action Login , SaveLogin

---
+++ FitCalorie/Controllers/AccountController.cs
@@ -0,0 +1,115 @@
+﻿using FitCalorie.Models;
+using FitCalorie.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FitCalorie.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveRegister(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register", register);
+            }
+
+
+            ApplicationUser applicationUser = new ApplicationUser
+            {
+                UserName = register.Email,
+                Email = register.Email,
+                Age = register.Age,
+                Height = register.Height,
+                Weight = register.Weight
+            };
+
+            IdentityResult result = await userManager.CreateAsync(applicationUser, register.Password);
+
+            if (result.Succeeded)
+            {
+
+                await signInManager.SignInAsync(applicationUser, isPersistent: false);
+                return RedirectToAction("LogMeal", "DailyLogs");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View("Register", register);
+
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Login(string? Url = null)
+        {
+            ViewData["ReturnUrl"] = Url;
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginView, string? returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login");
+            }
+
+
+            var result = await signInManager.PasswordSignInAsync(
+        userName: loginView.Email,
+        password: loginView.Password,
+        isPersistent: loginView.RememberMe,
+        lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+
+                if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                return RedirectToAction("LogMeal", "DailyLogs");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt. Please check your email or password.");
+            return View(loginView);
+
+        }
+    }
+}
---
+++ FitCalorie/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitCalorie.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+       
+
+        
+
+
+        [Required(ErrorMessage = "Age is required")]
+        [Range(10, 100, ErrorMessage = "Age must be between 10 and 100")]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "Height is required")]
+        [Range(100, 250, ErrorMessage = "Height must be between 100cm and 250cm")]
+        [Display(Name = "Height (cm)")]
+        public double Height { get; set; }
+
+
+        [Required(ErrorMessage = "Weight is required")]
+        [Range(30, 300, ErrorMessage = "Weight must be between 30kg and 300kg")]
+        [Display(Name = "Weight (kg)")]
+        public double Weight { get; set; }
+
+
+
+
+
+    }
+}
